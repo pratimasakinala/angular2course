805e7c8b0efc46ae3addba4a7cc487bf0fd07c47
Pratima Sakinala <pratimasakinala@Pratimas-MacBook-Pro.local>
2018-05-24 15:07:19 -0700

    adds day 3 content (mostly assignments)

--- src/app/lessons/week-1/day-3/day-3.component.ts
+++ src/app/lessons/week-1/day-3/day-3.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'day-3',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./day-3.component.css']
 })
 export class Day3Component implements OnInit {
+  @Output() scrolledTo = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  scrollIntoView(id) {
+    this.scrolledTo.emit(id);
   }
 
 }
