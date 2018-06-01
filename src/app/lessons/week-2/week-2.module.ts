b2e2d9bb04c0602116865a2781011bc31cab7e20
Pratima Sakinala <pratimasakinala@Pratimas-MacBook-Pro.local>
2018-05-31 17:11:58 -0700

    releases day 4 content. adds services content in week2 day 1.

--- src/app/lessons/week-2/week-2.module.ts
+++ src/app/lessons/week-2/week-2.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { Day1Component } from './day-1/day-1.component';
 import { Week2Component } from './week-2/week-2.component';
 import { Day2Component } from './day-2/day-2.component';
+import { Day3Component } from './day-3/day-3.component';
 
 @NgModule({
   imports: [
@@ -12,7 +13,8 @@ import { Day2Component } from './day-2/day-2.component';
   declarations: [
     Day1Component,
     Week2Component,
-    Day2Component
+    Day2Component,
+    Day3Component
   ],
   exports: [
     Week2Component
