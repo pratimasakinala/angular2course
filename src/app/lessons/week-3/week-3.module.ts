4b649a909b94047552d2d9db8236dd2cf69421a5
Pratima Sakinala <pratimasakinala@Pratimas-MacBook-Pro.local>
2018-06-07 09:00:50 -0700

    adds another column to lessons menu for when week-3 is ready.

--- src/app/lessons/week-3/week-3.module.ts
+++ src/app/lessons/week-3/week-3.module.ts
@@ -4,6 +4,9 @@ import { CommonModule } from '@angular/common';
 import { Week3Component } from './week-3/week-3.component';
 import { Day1Component } from './day-1/day-1.component';
 import { Day2Component } from './day-2/day-2.component';
+import { Day3Component } from './day-3/day-3.component';
+import { Day4Component } from './day-4/day-4.component';
+import { Day5Component } from './day-5/day-5.component';
 
 @NgModule({
   imports: [
@@ -12,7 +15,10 @@ import { Day2Component } from './day-2/day-2.component';
   declarations: [
     Week3Component,
     Day1Component,
-    Day2Component
+    Day2Component,
+    Day3Component,
+    Day4Component,
+    Day5Component
   ],
   exports: [
     Week3Component
