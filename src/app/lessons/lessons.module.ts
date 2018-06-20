f18c0be6d0361541234a6f402784566a903d983f
Pratima Sakinala <pratimasakinala@Pratimas-MacBook-Pro.local>
2018-06-19 17:07:16 -0700

    imports week 5 module in lessons module

--- src/app/lessons/lessons.module.ts
+++ src/app/lessons/lessons.module.ts
@@ -8,6 +8,7 @@ import { Week1Module } from './week-1/week-1.module';
 import { Week2Module } from './week-2/week-2.module';
 import { Week3Module } from './week-3/week-3.module';
 import { Week4Module } from './week-4/week-4.module';
+import { Week5Module } from './week-5/week-5.module';
 
 import { LessonsComponent } from './lessons/lessons.component';
 
@@ -18,7 +19,8 @@ import { LessonsComponent } from './lessons/lessons.component';
     Week1Module,
     Week2Module,
     Week3Module,
-    Week4Module
+    Week4Module,
+    Week5Module
   ],
   declarations: [
     LessonsComponent
