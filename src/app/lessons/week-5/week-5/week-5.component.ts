0c7b21989e2eaf70c073afb5a6857445c2a6fd32
Pratima Sakinala <pratimasakinala@Pratimas-MacBook-Pro.local>
2018-06-19 17:06:39 -0700

    creates week-5 module

---
+++ src/app/lessons/week-5/week-5/week-5.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-week-5',
+  templateUrl: './week-5.component.html',
+  styleUrls: ['./week-5.component.css']
+})
+export class Week5Component implements OnInit {
+  @Input() day: number;
+  @Output() scrolledTo = new EventEmitter();
+  
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      this.day = params['day'];
+    });
+  }
+
+  scrollIntoView(event) {
+    this.scrolledTo.emit(event);
+  }
+
+}
